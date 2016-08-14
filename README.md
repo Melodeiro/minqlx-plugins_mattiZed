01bf4f5213d2b5bc88be2d23ef30039c7024d107
Melodeiro <d1b@bk.ru>
2016-08-15 00:12:16 +0300

    Added non-round-based gametypes support and instant warning. Added version info and link for the plugin updater. Updated the readme.

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # minqlx-plugins
 This is a collection of plugins that I wrote for Mino's Quake Live Server Mod [minqlx](https://github.com/MinoMino/minqlx). If you run into problems or find any bugs, you can contact me via IRC: [Mino's Channel on Quakenet](http://webchat.quakenet.org/?channels=minqlbot). Look out for "mattiZed" - I may not respond immediately but I will sure read your messages.
 
+Recent versions of queue.py and uneventeams.py are supported by Melodeiro#6341 at Discord, or post your messages at the [SAM support channel](https://discordapp.com/channels/163619179923111937/166341692792766466), or just use [this form](https://github.com/Melodeiro/minqlx-plugins_mattiZed/issues/new) to create a github issue.
+
 ## queue.py
-This is a queue plugin. Some parts of it were inspired by the original queueinfo plugin which was
-written by [walkerX](https://github.com/WalkerY/minqlbot-plugins/tree/queueinfo/plugins) for the old minqlbot.
+This is a queue plugin. Originally written by [mattiZed](https://github.com/mattiZed/minqlx-plugins), now with a whole different mechanics, which should fully replace the old one.
 
-The plugin basically shows for how long people have been waiting in specator
-mode. If a player joins a team, the name is kept for three minutes (so admins
-can track players that dont respect the queue) in the list but now displayed
-with an asterisk to show that the player has left the queue and will soon be
-removed.
+From 2.0 version this plugin implements duel-style queue into the all gametypes. Players receive a status, which is displayed in a round brackets: (s) - spectating, (1..2..n) - position in queue, (AFK) - afk status. For team-based gametypes plugin waits for 2 players before adding them into the game in progress. Also players being added to queue if there is no place in game (due teamsize or locked teams)
 
-The plugin also features an AFK list, to which players can 
-subscribe/unsubscribe to.
+* **qlx_queueSetAfkPermission "2"** - minimum permission level for setting afk status to other players
+* **qlx_queueAFKTag "^3AFK"** - tag used for the afk status
+* **!q** - show all players from queue
+* **!afk** - set your status to AFK (spectators only)
+* **!here** - remove your AFK status
+
+**NOTE:** for correctly updating the player tags after using the !clan, use the [modified clan.py plugin](https://github.com/Melodeiro/minqlx-plugins_MinoMino/blob/master/clan.py)
 
 ## pummel.py
 This is a fun plugin.
@@ -20,13 +22,20 @@ This is a fun plugin.
 It displays "Killer x:y Victim" message when Victim gets killed with gauntlet
 and stores the information in REDIS DB
 
-Players can display their "pummels" via !pummel - this works only for victims
+Players can display their "pummels" via **!pummel** - this works only for victims
 that are on the server at the same time, otherwise we could just spit out
 steamIDs.
 
 ## uneventeams.py
 This plugin takes care of uneven teams.
 
-If uneven teams occur this plugin finds the player who has played the least amount of time since he connected. The information stays persistant over mapchanges etc. In this context playing time means for how long the players have been in a team in an ACTIVE GAME, no matter how long they were alive, though.
+If uneven teams occur this plugin finds the player who has played the least amount of time since he connected. The information stays persistant over mapchanges etc. In this context playing time means for how long the players have been in a team in an ACTIVE GAME, no matter how long they were alive, though. Keeps timers for 180 seconds, if players disconnected or moved to spectators.
 
 Some parts of this plugin were inspired by this autospec plugin written by [iou(onegirl)](https://github.com/dsverdlo/minqlx-plugins/blob/master/autospec.py), but the decision mechanism that takes care of who will be "punished" is a different approach.
+
+From 1.8 version with non-round-based gametypes support! For full support use with queue.py version 2.0 or later.
+
+* **qlx_unevenTeamsAction "0"** - 0 to slay or 1 to move to spectators when teams are uneven
+* **qlx_unevenTeamsMinPlayers "2"** - minimum amount of players in red + blue for uneventeams to work
+* **qlx_unevenTeamsActionDelay "15"** - delay (seconds) before excess player will be handled in non round-based gamemodes
+* **qlx_unevenTeamsInstantWarning "0"** - if set to 1, don't wait for next round for checking and moving to spectators. Not available for certain gametypes
--- uneventeams.py
+++ uneventeams.py
@@ -31,10 +31,16 @@
 # Please consider this a very early version and highly experimental. 
 # I haven't tested it that much yet because testing these things is hard...
 
+# Now with non-round-based gametypes support. For handling the new joined players use the
+# new queue plugin: https://github.com/Melodeiro/minqlx-plugins_mattiZed/blob/master/queue.py
+
 import minqlx
 import datetime
 import time
 import threading
+
+NON_ROUND_BASED_GAMETYPES = ("ctf", "dom", "tdm", "1f", "har")
+SLAY_NOT_SUPPORTED_GAMETYPES = ("ft", "dom", "tdm", "1f", "har")
 
 class timer():
     def __init__(self, running=False):
@@ -71,24 +77,37 @@ class timer():
 
 class uneventeams(minqlx.Plugin):
     def __init__(self):
+        self.add_hook("new_game", self.handle_new_game)
+        self.add_hook("game_end", self.handle_game_end)
         self.add_hook("player_connect", self.handle_player_connect)
         self.add_hook("team_switch", self.handle_team_switch)
         self.add_hook("player_disconnect", self.handle_player_disconnect)
         self.add_hook("round_start", self.handle_round_start)
-        #self.add_hook("round_end", self.handle_round_end)
         self.add_hook("round_countdown", self.handle_round_countdown)
-        #self.add_hook("game_end", self.handle_game_end)
+        self.add_hook("game_start", self.handle_game_start)
+        self.add_hook("game_countdown", self.handle_game_countdown)
         self.add_command("playertimes", self.cmd_playertimes, 2)
+        self.add_command("utversion", self.cmd_utversion)
         
         # { steam_id : time_played }
         self._players = {}
+        self.is_endscreen = False
+        
+        self.version = 1.8
+        self.plugin_updater_url = "https://raw.githubusercontent.com/Melodeiro/minqlx-plugins_mattiZed/master/uneventeams.py"
         
         self.initialize()
         
-        # Slay (0) or move to spectators (1) when teams are uneven
+        
+        # 0 to slay or 1 to move to spectators when teams are uneven
         self.set_cvar_once("qlx_unevenTeamsAction", "0")
         # Minimum amount of players in red + blue for uneventeams to work
         self.set_cvar_once("qlx_unevenTeamsMinPlayers", "2")
+        # Delay (seconds) before excess player will be handled in non round-based gamemodes
+        self.set_cvar_once("qlx_unevenTeamsActionDelay", "15")
+        # If qlx_unevenTeamsInstantWarning set to 1, don't wait for next round for checking 
+        # and moving to spectators. Not available for certain gametypes
+        self.set_cvar_once("qlx_unevenTeamsInstantWarning", "0")
         
     def initialize(self):
         '''
@@ -105,84 +124,90 @@ class uneventeams(minqlx.Plugin):
         for p in players["spectator"]:
             self._players[p.steam_id] = timer()
     
-    def handle_round_countdown(self, round_number):
+    @minqlx.next_frame
+    def check_teams(self, punish = False, old_guy = None):
         '''
             Check if teams are uneven and if so, warn the player with the 
-            least amount of time played.
+            least amount of time played, or punish if variable set to True.
         '''
+        @minqlx.thread
+        def deferred_punish(guy):
+            '''
+                Perform an action on excess player with delay.
+            '''
+            delay = self.get_cvar("qlx_unevenTeamsActionDelay", int)
+            time.sleep(delay)
+            self.check_teams(True, guy)
+        
         min_players = self.get_cvar("qlx_unevenTeamsMinPlayers", int)
+        instant_warning = self.get_cvar("qlx_unevenTeamsInstantWarning", int)
         
         teams = self.teams()
         
+        if self.is_endscreen:
+            return
         if len(teams["red"]) == len(teams["blue"]):
             return
         if len(teams["red"] + teams["blue"]) < min_players:
             return
-        
         if len(teams["red"]) > len(teams["blue"]):
             guy = self.player(self.find_lastjoined("red"))
         else:
             guy = self.player(self.find_lastjoined("blue"))
-        self.msg("^1Uneven Teams^7 >> {}^7 joined last and should spectate".format(guy.name))
+            
+        # cancel the punishment if last joined guy has changed (only for instant warning)
+        if instant_warning == 1 or self.game.type_short in NON_ROUND_BASED_GAMETYPES:
+            if old_guy and old_guy != guy:
+                punish = False
+            
+        if punish:
+            action = self.get_cvar("qlx_unevenTeamsAction", int)
+            
+            if action == 1 or self.game.type_short in SLAY_NOT_SUPPORTED_GAMETYPES:
+                guy.put("spectator")
+                try:
+                    # adding the guy to the first place of queue (needs new queue.py plugin installed)
+                    queue = minqlx.Plugin._loaded_plugins['queue']
+                    queue.addToQueue(guy, 0)
+                    self.msg("^1Uneven Teams^7 >> {}^7 was moved to queue.".format(guy.name))
+                except:
+                    self.msg("^1Uneven Teams^7 >> {}^7 was moved to spectators.".format(guy.name))
+            elif action == 0:
+                guy.health = 0
+                self.msg("^1Uneven Teams^7 >> {}^7 was slain.".format(guy.name))
+        else:
+            self.msg("^1Uneven Teams^7 >> {}^7 joined last and should spectate".format(guy.name))
+            # punish the guy after certain delay if instant warning is enabled
+            if instant_warning == 1 or self.game.type_short in NON_ROUND_BASED_GAMETYPES:
+                deferred_punish(guy)
+                
+    def handle_game_countdown(self):
+        if self.game.type_short in NON_ROUND_BASED_GAMETYPES:
+            self.check_teams()
+        
+    def handle_game_start(self, data):
+        if self.game.type_short in NON_ROUND_BASED_GAMETYPES:
+            self.check_teams(True)
+    
+    def handle_round_countdown(self, round_number):
+        '''
+            Warn excess player at the end of round.
+        '''
+        instant_warning = self.get_cvar("qlx_unevenTeamsInstantWarning", int)
+        if instant_warning == 0:
+            self.check_teams()
     
     def handle_round_start(self, round_number):
         '''
-            Start (or resume) the timers for the players in RED and BLUE.
-            Check also if we need to slay someone.
+            Kick/move to spectators excess player at the start of round.
         '''
-        min_players = self.get_cvar("qlx_unevenTeamsMinPlayers", int)
-        action = self.get_cvar("qlx_unevenTeamsAction", int)
-        
-        teams = self.teams()
-        
         for p in self._players.keys():
             try: 
                 self.player(p)
             except:
-                del self._players[p]
+                self.deferred_removing(p, self._players[p.steam_id].elapsed())
         
-        if len(teams["red"]) == len(teams["blue"]):
-            return
-        if len(teams["red"] + teams["blue"]) < min_players:
-            return
-        
-        if len(teams["red"]) > len(teams["blue"]):
-            guy = self.player(self.find_lastjoined("red"))
-        else:
-            guy = self.player(self.find_lastjoined("blue"))
-        
-        if action == 0:
-            guy.health = 0
-            self.msg("^1Uneven Teams^7 >> {}^7 was slain.".format(guy.name))
-        if action == 1:
-            guy.put("spectator")
-            try:
-                queue = minqlx.Plugin._loaded_plugins['queue']
-                queue.addToQueue(guy, 0)
-                self.msg("^1Uneven Teams^7 >> {}^7 was moved to queue.".format(guy.name))
-            except:
-                self.msg("^1Uneven Teams^7 >> {}^7 was moved to spectators.".format(guy.name))
-        
-    def handle_round_end(self, round_number):
-        '''
-            The round is over, stop the players' timers.
-        '''
-        teams = self.teams()
-        
-        for p in teams["red"] + teams["blue"]:
-            self._players[p.steam_id].stop()
-    
-    def handle_game_end(self, data):
-        '''
-            The game has ended for any reason. Stop all timing.
-        '''
-        teams = self.teams()
-        
-        for p in teams["red"]:
-            self._players[p.steam_id].stop()
-        
-        for p in teams["blue"]:
-            self._players[p.steam_id].stop()
+        self.check_teams(True)
     
     def handle_team_switch(self, player, old_team, new_team):
         '''
@@ -191,6 +216,11 @@ class uneventeams(minqlx.Plugin):
         if new_team == "spectator":
             self._players[player.steam_id].stop()
             self.deferred_removing(player, self._players[player.steam_id].elapsed())
+            
+            instant_warning = self.get_cvar("qlx_unevenTeamsInstantWarning", int)
+            if self.game.state == 'in_progress':
+                if self.game.type_short in NON_ROUND_BASED_GAMETYPES or instant_warning == 1:
+                    self.check_teams()
         
         if new_team == "red" or new_team == "blue":
             self._players[player.steam_id].start()
@@ -199,6 +229,11 @@ class uneventeams(minqlx.Plugin):
         if player.steam_id in self._players.keys():
             self._players[player.steam_id].stop()
             self.deferred_removing(player, self._players[player.steam_id].elapsed())
+        
+        instant_warning = self.get_cvar("qlx_unevenTeamsInstantWarning", int)
+        if self.game.state == 'in_progress':
+            if self.game.type_short in NON_ROUND_BASED_GAMETYPES or instant_warning == 1:
+                self.check_teams()
     
     def handle_player_connect(self, player):
         '''
@@ -265,4 +300,11 @@ class uneventeams(minqlx.Plugin):
         if player.steam_id in self._players.keys():
             removing()
             
-            
+    def cmd_utversion(self, player, msg, channel):
+        channel.reply('^7This server has installed ^2uneventeams.py {} ^7ver. by matti^1Z^7ed'.format(self.version))
+    
+    def handle_new_game(self):
+        self.is_endscreen = False
+        
+    def handle_game_end(self, data):
+        self.is_endscreen = True
